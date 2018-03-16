19e312005ec3c451655d7bb95da35e8e0ac02ee6
dylanling <dylan.ling@lendup.com>
2018-03-15 17:54:41 -0700

    cleanup

--- src/expressions/recurrence.ts
+++ src/expressions/recurrence.ts
@@ -6,4 +6,23 @@ export const weekly = (from: Moment) => from.clone().add(1, 'weeks')
 export const biweekly = (from: Moment) => from.clone().add(2, 'weeks')
 
 export const annually = (from: Moment) => from.clone().add(1, 'years')
-//export const monthlyOnThe = (date: number) => (from: Moment) =>
+
+export const monthlyOnThe = (date: number) => (from: Moment) => {
+  if (date < 1 || date > 28) {
+    throw new RangeError(
+      `cannot construct a monthly recurrence on the ${date} of the month`
+    )
+  }
+  return from.date() < date
+    ? from.clone().date(date)
+    : from.month() === 12
+      ? from
+          .clone()
+          .year(from.year() + 1)
+          .month(0)
+          .date(date)
+      : from
+          .clone()
+          .month(from.month() + 1)
+          .date(date)
+}
--- test/expressions/recurring.test.ts
+++ test/expressions/recurring.test.ts
@@ -2,18 +2,20 @@ import { during, recurring } from '../../src/expressions/expressions'
 import { List } from 'immutable'
 import { rangesEqual } from '../../src/expressions/ranges'
 import { DateRange } from 'moment-range'
-import { weekly } from '../../src/expressions/recurrence'
-import { monthOf } from './helper'
+import { weekly, monthlyOnThe } from '../../src/expressions/recurrence'
+import { monthOf, _2018 } from './helper'
+import { Moment } from 'moment'
 
 describe('Recurring', () => {
-  it('returns recurring weekly', () => {
-    const twoToThreePMOnMarch = (date: number) =>
-      during(
-        new DateRange(
-          new Date(Date.UTC(2018, 2, date, 14, 0, 0)),
-          new Date(Date.UTC(2018, 2, date, 15, 0, 0))
-        )
+  const twoToThreePMOnMarch = (date: number) =>
+    during(
+      new DateRange(
+        new Date(Date.UTC(2018, 2, date, 14, 0, 0)),
+        new Date(Date.UTC(2018, 2, date, 15, 0, 0))
       )
+    )
+
+  it('returns recurring weekly', () => {
     const march = monthOf(2018, 3)
 
     const expression = recurring(weekly, twoToThreePMOnMarch(1))
